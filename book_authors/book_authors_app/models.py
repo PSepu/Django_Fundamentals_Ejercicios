1f92c252a86ea01030c0221d796ccea02451ad32
Pablo Sepulveda <p.sepulvedamorande@gmail.com>
2022-02-13 19:46:57 -0300

    Book and Authors

---
+++ book_authors/book_authors_app/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"<Book: {self.title}>"
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    notes = models.TextField(default="No Notes")
+    books = models.ManyToManyField(Book, related_name="author")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"<Author: {self.first_name} {self.last_name}>"
